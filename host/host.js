584a2daeedeb137d40b9a20235925b0829bead5a
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-05-18 15:29:47 +0200

    Commented out unnecessary code

--- host/host.js
+++ host/host.js
@@ -21,14 +21,14 @@ socket.on('drawing', function(data)
   {
     drawing = data[0];
     name = data[1];
-    template = '<li id="'+name+'" class="player"><span class="player_name"></span><canvas class="player_icon" width="490" height="220"></canvas></li>';
+    template = '<li id="'+name+'" class="player"><span class="player_name"></span><canvas class="player_icon" width="'+drawing[4]+'" height="'+drawing[3]+'"></canvas></li>';
     $("#players").append(template);
     $("#"+name).find("canvas").attr("class", name);
     $("#"+name).find("span").html("Room code: "+name)
 
     context = document.getElementsByClassName(name)[0].getContext("2d"); //defines the new context variable to be used by main.redraw();
-    $("."+name).attr("height", drawing[3] + "px");
-    $("."+name).attr("width", drawing[4] + "px");
+    /*$("."+name).attr("height", drawing[3] + "px");
+    $("."+name).attr("width", drawing[4] + "px");*/
     main.redraw(drawing[0], drawing[1], drawing[2], drawing[5], true);
     clients.push([name, drawing]);
   }
