0f79ada4ef48d14afc0d56e68679e3c1fd6cec15
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-05-18 12:24:07 +0200

    Added nodejs httpserver, now serving only the files needed

--- server/server.js
+++ server/server.js
@@ -7,6 +7,29 @@ var crypto = require('crypto');
 var port = 3000;
 var lists = [];
 
+var static = require('node-static');
+var MobileDetect = require('mobile-detect');
+
+//
+// Create a node-static server instance to serve the './public' folder
+//
+var file = new static.Server('./');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        md = new MobileDetect(request.headers['user-agent']);
+
+        if(md.mobile() === null) type = "host";
+        else type = "mobile";
+
+        if(request.url == "/style.css") file.serveFile(type+'/style.css', 200, {}, request, response);
+        else if(request.url == "/lib/main.js") file.serveFile('lib/main.js', 200, {}, request, response);
+        else if(request.url == "/host.js" || request.url == "/mobile.js") file.serveFile(type+'/'+type+'.js', 200, {}, request, response);
+        else file.serveFile(type+'/index.html', 200, {}, request, response);
+
+    }).resume();
+}).listen(8080);
+
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
