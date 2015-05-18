5c5122a30ef0d80e1d20438a9dd20d641fea56a9
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-05-18 12:37:56 +0200

    Somethangs

--- server/server.js
+++ server/server.js
@@ -17,11 +17,10 @@ var file = new static.Server('./');
 
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
-        md = new MobileDetect(request.headers['user-agent']);
 
+        md = new MobileDetect(request.headers['user-agent']);
         if(md.mobile() === null) type = "host";
         else type = "mobile";
-
         if(request.url == "/style.css") file.serveFile(type+'/style.css', 200, {}, request, response);
         else if(request.url == "/lib/main.js") file.serveFile('lib/main.js', 200, {}, request, response);
         else if(request.url == "/host.js" || request.url == "/mobile.js") file.serveFile(type+'/'+type+'.js', 200, {}, request, response);
@@ -50,6 +49,7 @@ io.on('connection', function(socket){
           socket.join(room);
           break;
         case "client":
+          console.log('client present');
           socket.join('client');
           io.to(room).emit('client-join', guid);
           break;
